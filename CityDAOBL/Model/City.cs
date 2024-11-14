3a523d0b4cabb713602be2f3c6ac579a90d516d1
Tom Vande Wiele <tvdewiel@hotmail.com>
2024-11-14 09:31:23 +0100

    Add project files.

---
+++ CityDAOBL/Model/City.cs
@@ -0,0 +1,82 @@
+﻿using CityDAOBL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityDAOBL.Model
+{
+    public class City
+    {
+        public City(string name, string description, string country)
+        {
+            Name = name;
+            Description = description;
+            Country = country;
+        }
+        public City(string name, string description, string country, List<Attraction> attractions)
+        {
+            Name = name;
+            Description = description;
+            Country = country;
+            attractions = attractions;
+        }
+        public City(int id, string name, string description, string country)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Country = country;
+        }
+        public City(int id, string name, string description, string country, List<Attraction> attractions)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Country = country;
+            this.attractions = attractions;
+        }
+
+        public int? Id { get; set; }
+        private string name = "";
+        public string Name
+        {
+            get { return name; }
+            set { if (string.IsNullOrWhiteSpace(value)) throw new CityException("name invalid"); name = value; }
+        }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        private List<Attraction> attractions { get; set; } = new();
+        public IReadOnlyList<Attraction> Attractions => attractions;
+        public bool HasAttraction(Attraction attraction)
+        {
+            return attractions.Contains(attraction);
+        }
+        public void AddAttraction(Attraction attraction)
+        {
+            if (!HasAttraction(attraction)) attractions.Add(attraction);
+            else throw new CityException("AddAttraction");
+        }
+        public void RemoveAttraction(Attraction attraction)
+        {
+            if (!HasAttraction(attraction)) throw new CityException("RemoveAttraction");
+            attractions.Remove(attraction);
+        }
+        public string ShowCity()
+        {
+            string x = $"Welcome to {Name}, located in {Country}"
+                + $"\n\nThis city can be described as {Description}";
+            return x;
+        }
+        public string ShowCityAttractions()
+        {
+            string x = $"We have to offer {Attractions.Count} attractions";
+            foreach (Attraction attraction in Attractions)
+            {
+                x += "\n" + attraction.ShowShortDescription();
+            }
+            return x;
+        }
+    }
+}
---
+++ CityRepoBL/Model/Attraction.cs
@@ -0,0 +1,78 @@
+﻿using CityRepoBL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityRepoBL.Model
+{
+    public class Attraction
+    {
+        public Attraction(string name, string description, string type, string location, string organizer)
+        {
+            Name = name;
+            Description = description;
+            Type = type;
+            Location = location;
+            Organizer = organizer;
+        }
+        public Attraction(int iD, string name, string description, string type, string location, string organizer)
+        {
+            Id = iD;
+            Name = name;
+            Description = description;
+            Type = type;
+            Location = location;
+            Organizer = organizer;
+        }
+
+        public int? Id { get; set; }
+        private string name = "";
+        public string Name
+        {
+            get { return name; }
+            set { if (string.IsNullOrWhiteSpace(value)) throw new CityException("name invalid"); name = value; }
+        }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        private string location;
+        public string Location
+        {
+            get { return location; }
+            set { if (string.IsNullOrWhiteSpace(value)) throw new CityException("location invalid"); location = value; }
+        }
+        private string organizer;
+        public string Organizer
+        {
+            get { return organizer; }
+            set { if (string.IsNullOrWhiteSpace(value)) throw new CityException("organizer invalid"); organizer = value; }
+        }
+        public string ShowShortDescription()
+        {
+            return $"{Name} is of type {Type} and located at {Location}";
+        }
+        public string ShowFullDescription()
+        {
+            return $"{Name} is of type {Type}, organized by {Organizer} and located at {Location}"
+                + $"\nDetails : {Description}";
+        }
+        public override bool Equals(object? other)
+        {
+            if (other is Attraction)
+            {
+                Attraction compAttraction = (Attraction)other;
+                if (Id.HasValue && compAttraction.Id.HasValue)
+                {
+                    if (Id == compAttraction.Id) return true; else return false;
+                }
+                else
+                {
+                    return Name == compAttraction.Name &&
+                   Organizer == compAttraction.Organizer;
+                }
+            }
+            else { return false; }
+        }
+    }
+}
